8c43775fee790bb0f46b5beaa6ecaeb7781b05fd
Idan <idangibly.dev@gmail.com>
2024-10-15 10:17:25 +0300

    test bash scripts

--- release.js
+++
@@ -1,8 +0,0 @@
-const { execSync } = require('child_process');
-const oldVersion = require('./package.json').version;
-execSync(`git add . && git commit -m "release patch ${oldVersion} (last version)"`);
-execSync('npm version patch');
-const version = require('./package.json').version;
-execSync(`git add . && git commit -m "release ${version}"`);
-execSync(`git tag v${version}`);
-execSync('git push');
