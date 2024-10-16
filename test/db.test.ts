e79e0464f3d1a871c43bd91c91b46ca086109a20
Idan <idangibly.dev@gmail.com>
2024-10-16 11:31:52 +0300

    tests for release

--- test/db.test.ts
+++ test/db.test.ts
@@ -1,28 +1,41 @@
 import { PostgreSqlClient } from '../src/database/postgresql.client';
 import { describe, test, expect } from '@jest/globals';
-
-describe('Test db', () => {
-    test('1. Test Postgresql client init: Fail', async () => {
-      const postgresql = new PostgreSqlClient();
-      const result = await postgresql.init({ 
-        host: '', 
-        password: '', 
-        port: 2, 
-        username: '',
-        database: '' 
+import { config } from 'dotenv';
+import { resolve } from 'path';
+config({ path: resolve(process.cwd(), '.env.test') });
+const INCLUDE_DB_CONNECTION_TESTS = '1';
+describe('Tests', () => {
+    if (process.env.TEST_INCLUDE_DB_CONNECTION_TESTS === INCLUDE_DB_CONNECTION_TESTS) {
+      describe('Test db', () => {
+        test('1. Test Postgresql client init: Fail', async () => {
+          const postgresql = new PostgreSqlClient();
+          const result = await postgresql.init({ 
+            host: '', 
+            password: '', 
+            port: 2, 
+            username: '',
+            database: '' 
+          });
+          expect(result).toBe(false);
+        });
+    
+        test('2. Test Postgresql client init: Success', async () => {
+          const postgresql = new PostgreSqlClient();
+          const result = await postgresql.init({ 
+            host: process.env.TEST_DB_POSTGRESQL_HOST ?? '', 
+            password: process.env.TEST_DB_POSTGRESQL_PASSWORD ?? '',
+            port: Number(process.env._TEST_DB_POSTGRESQL_PORT ?? 0), 
+            username: process.env.TEST_DB_POSTGRESQL_USERNAME ?? '',
+            database: process.env.TEST_DB_POSTGRESQL_DATABASE ?? '',
+          });
+          expect(result).toBe(true);
+        });
       });
-      expect(result).toBe(false);
+    }
+    describe('Test utils', () => {
+      test('1. Test 1=1', () => {
+        expect(1).toBe(1);
+      });
     });
 
-    test('2. Test Postgresql client init: Success', async () => {
-      const postgresql = new PostgreSqlClient();
-      const result = await postgresql.init({ 
-        host: process.env.TEST_DB_POSTGRESQL_HOST ?? '', 
-        password: process.env.TEST_DB_POSTGRESQL_PASSWORD ?? '',
-        port: Number(process.env._TEST_DB_POSTGRESQL_PORT ?? 0), 
-        username: process.env.TEST_DB_POSTGRESQL_USERNAME ?? '',
-        database: process.env.TEST_DB_POSTGRESQL_DATABASE ?? '',
-      });
-      expect(result).toBe(true);
-    });
 });
